9a89b5b046cf5df26aba14425d46496fe2e0faef
Alan Lunazzi <alan.lunazzi@mercadolibre.com>
2019-07-26 23:36:45 -0300

    Se modifican nombres de variables

--- scrit.py
+++ scrit.py
@@ -6,16 +6,15 @@ today = datetime.today().strftime('%Y%m%d')
 namefile = 'publicaciones' + today + '.csv'
 c = csv.writer(open(namefile , "w"), delimiter=';', lineterminator = '\n')
 token = 'APP_USR-7278287269671778-072623-c6d4136fb211d41df48c4179cb7c1cbb-135124707'
-respuestas = []
-descriptions = []
-text_description = []
+itemResponse = []
+descriptionResponse = []
 
 def saveItemsInCsv(results):
 	print ("Se escribe archivo por primera vez")
 	c.writerow(['Item', 'Titulo', 'Descripcion', 'Precio', 'Imagen 1', 'Imagen 2', 'Imagen 3', 'Imagen 4', 'Imagen 5']) 
-	for urls in results:
-		print ("Se guarda resultado del item " + urls[8])
-		c.writerow([urls[8], unicode(urls[5]).encode('utf-8'), unicode(urls[6]).encode('utf-8') , urls[7], urls[0], urls[1], urls[2], urls[3], urls[4]]) 
+	for itemInfo in results:
+		print ("Se guarda resultado del item " + itemInfo[8])
+		c.writerow([itemInfo[8], unicode(itemInfo[5]).encode('utf-8'), unicode(itemInfo[6]).encode('utf-8') , itemInfo[7], itemInfo[0], itemInfo[1], itemInfo[2], itemInfo[3], itemInfo[4]]) 
 	print("Archivo " + namefile + " cargado correctamente! =)")
 
 def loadItems():
@@ -30,27 +29,27 @@ def callApis(array):
 	i = 1
 	for item in array:
 		print('Llamamos al item ' + str(i) + ' de ' + str(len(array)) + ' ' + item)
-		respuestas.append(requests.get('https://api.mercadolibre.com/items/' + item + '?access_token=' + token).json())
-		descriptions.append(requests.get('https://api.mercadolibre.com/items/' + item + '/description' + '?access_token=' + token).json())
+		itemResponse.append(requests.get('https://api.mercadolibre.com/items/' + item + '?access_token=' + token).json())
+		descriptionResponse.append(requests.get('https://api.mercadolibre.com/items/' + item + '/description' + '?access_token=' + token).json())
 		i += 1
 
 def parseItems(array):
 	i = 0
-	urls = []
+	itemInfo = []
 	results = []
-	for r in respuestas:
-		if('pictures' in r and 'title' in r and 'plain_text' in descriptions[i]):
+	for r in itemResponse:
+		if('pictures' in r and 'title' in r and 'plain_text' in descriptionResponse[i]):
 			for picture in r['pictures']:
-				urls.append(str(picture['url']))
-			urls.insert(5,r['title'])
-			urls.insert(6,descriptions[i]['plain_text'])
-			urls.insert(7,r['price'])
-			urls.insert(8,array[i])
-			results.append(urls)
-			urls = [] 
+				itemInfo.append(str(picture['url']))
+			itemInfo.insert(5,r['title'])
+			itemInfo.insert(6,descriptionResponse[i]['plain_text'])
+			itemInfo.insert(7,r['price'])
+			itemInfo.insert(8,array[i])
+			results.append(itemInfo)
+			itemInfo = [] 
 		else:
 			print('El item ' + array[i] + ' no fue encontrado o no contenia algun campo obligatorio.'
-			+ ' mensaje de error:' + str(r) + str(descriptions[i]))
+			+ ' mensaje de error:' + str(r) + str(descriptionResponse[i]))
 		i += 1
 	return results
 
